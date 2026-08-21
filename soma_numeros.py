f501d0a13948674099096b32db3cea7f39d59258
Raphaela Buin <raphaeladasilvabuin@gmail.com>
2026-08-20 21:59:28 -0300

    feat: exercicio 01

---
+++ soma_numeros.py
@@ -0,0 +1,15 @@
+#ENTRADA DE CODIGOS
+primeiro_valor = int(input("Digite o primeiro número: "))
+segundo_valor = int(input("Digite o segundo número: "))
+
+#PROCESSAMENTO
+soma = primeiro_valor + segundo_valor
+
+#SAÍDA
+print(f"Soma: {soma}")
+
+
+#VALORES DE TESTE
+# Teste 1: 4 e 9
+# Teste 2: -3 e 8
+# Teste 3: 0 e 12
