b0e2dec995554e4bccc46fc970c3365fa5e0dc67
Raphaela Buin <raphaeladasilvabuin@gmail.com>
2026-08-22 13:43:02 -0300

    Exercicio 03 - Antecessor e Sucessor

---
+++ antecessor_sucessor.py
@@ -0,0 +1,25 @@
+#ENTRADA DE DADOS
+numero = int(input("Digite um número inteiro: "))
+
+#PROCESSAMENTO
+antecessor = numero - 1
+sucessor = numero +1
+
+#SAÍDA DE DADOS
+print(f"Antecessor: {antecessor}")
+print(f"Número: {numero}")
+print(f"Sucessor: {sucessor}")
+
+#VALORES DE TESTE
+#Entrada: 10
+#Antecessor: 9
+#Sucessor: 11
+#Entrada: 1
+#Antecessor: 0
+#Sucessor: 2
+#Entrada: 0
+#Antecessor: -1
+#Sucessor: 1
+#Entrada: -7
+#Antecessor: -8
+#Sucessor: -6
